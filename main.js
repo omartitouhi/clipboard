db7ff95f52c57d0cc76d5744af2e7419a38a2d46
omar <titouhiomar223@gmail.com>
2023-08-23 19:45:49 +0100

    added some css and jquery functions

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+    //two function to add and remove the class in the span
+    function add(){
+        $(".copied").addClass("bounce-effect");
+    }
+    function remove(){
+        $(".copied").removeClass("bounce-effect");
+    }
+
+    $(".copy-btn").click(function (){
+        $("#textField").select();
+        document.execCommand("copy");
+        add();
+        setTimeout(remove,800);
+    });
+});
